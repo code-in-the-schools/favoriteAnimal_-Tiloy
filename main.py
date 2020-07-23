0957cfd184575f33e8ac3bbcf4634c3a8c6c305f
Tiloy84 <tiloy67@gmail.com>
2020-07-23 12:43:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+Q = int(input("Enter any number 1-4 to learn about my favorite animal"))
+if(Q == 1):
+  print(" I have a dog nammed chopper")
+  elif(Q == 2)
+  print(" My animal has a very good sense of smell")
+  elif(Q == 3)
+  print(" My animal is a conivorian")
+  elif(Q == 4)
+  print("My animal allways knows if something bad is going on")
